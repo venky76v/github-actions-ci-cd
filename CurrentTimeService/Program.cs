c7868df71c0bca981b8a5967494a2f6ab4abe78b
Venky Venkataraman <venkateshv76@hotmail.com>
2024-11-11 01:25:30 +0000

    Test commit

--- CurrentTimeService/Program.cs
+++ CurrentTimeService/Program.cs
@@ -13,4 +13,7 @@ app.UseHttpsRedirection();
 // GET UTC and check if this works
 app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
 
+// GET DateTime Now and check if this works
+app.MapGet("time/default", () => Results.Ok(DateTime.Now));
+
 await app.RunAsync();
